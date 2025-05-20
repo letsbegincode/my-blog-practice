a5fc5c048925666503fd64fb278e0305f1e3b425
letsbegincode <134317648+letsbegincode@users.noreply.github.com>
2025-05-20 15:18:42 +0530

    Add proper testing setup with mocha and chai

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
-const assert = require('assert');
+const { expect } = require('chai');
 
-describe('Sample Test', () => {
-  it('should pass', () => {
-    assert.strictEqual(1 + 1, 2);
+describe('Sample Test Suite', () => {
+  it('should add numbers correctly', () => {
+    expect(1 + 1).to.equal(2);
+  });
+  
+  it('should verify lodash is working', () => {
+    const _ = require('lodash');
+    expect(_.isEmpty({})).to.be.true;
   });
 });
